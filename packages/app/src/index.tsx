fad85c92b9f8a1c78bcc955337af21810e892aec
MenSeb <sebastien.menard.github@gmail.com>
2023-03-17 18:00:56 -0400

    chore(styles): create and import styles reset

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import './styles/utilities/reset';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
