3b8881c6a4a40eac192e68ad60dc073b03a8e8fd
MenSeb <sebastien.menard.github@gmail.com>
2023-03-17 18:10:43 -0400

    chore(styles): export styles in index file and import them at root

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import './styles/utilities/reset';
+import './styles';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
