fd9acf1bb24541d0da12005664588876a67d5435
MenSeb <sebastien.menard.github@gmail.com>
2023-03-19 11:11:36 -0400

    feat(language): create context language

---
+++ packages/app/src/contexts/Language.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+type Language = 'english' | 'french';
+
+type LanguageToggle = (
+  event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+) => void;
+
+type LanguageState = {
+  language: Language;
+  toggleLanguage: LanguageToggle;
+};
+
+export const LanguageContext = React.createContext<LanguageState>(
+  {} as LanguageState,
+);
+
+export function LanguageProvider({
+  children,
+}: React.PropsWithChildren): JSX.Element {
+  const [language, setLanguage] = React.useState<Language>('french');
+
+  const toggleLanguage = React.useCallback<LanguageToggle>((event) => {
+    event.preventDefault();
+
+    setLanguage((currentLanguage) =>
+      currentLanguage === 'french' ? 'english' : 'french',
+    );
+  }, []);
+
+  return (
+    <LanguageContext.Provider value={{ language, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
