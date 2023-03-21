189d62d9484e3f6bb9f4dbbd9d4c938e4ec235d9
MenSeb <sebastien.menard.github@gmail.com>
2023-03-21 13:24:10 -0400

    refactor(theme): update logic with active and inactive theme

--- packages/app/src/contexts/theme.tsx
+++ packages/app/src/contexts/theme.tsx
@@ -2,16 +2,22 @@ import * as React from 'react';
 
 type Theme = 'light' | 'dark';
 
+type ThemeState = {
+  active: Theme;
+  inactive: Theme;
+};
+
 type ThemeToggle = () => void;
 
 type ThemeStore = {
-  theme: Theme;
+  theme: ThemeState;
   toggleTheme: ThemeToggle;
 };
 
 export const defaultTheme: Theme = 'light';
 
-export const ThemeContext = React.createContext<ThemeStore>({} as ThemeStore);
+export const ThemeContext: React.Context<ThemeStore> =
+  React.createContext<ThemeStore>({} as ThemeStore);
 
 export function useThemeContext(): ThemeStore {
   return React.useContext<ThemeStore>(ThemeContext);
@@ -20,10 +26,16 @@ export function useThemeContext(): ThemeStore {
 export function ThemeProvider({
   children,
 }: React.PropsWithChildren): JSX.Element {
-  const [theme, setTheme] = React.useState<Theme>(defaultTheme);
+  const [theme, setTheme] = React.useState<ThemeState>({
+    active: defaultTheme,
+    inactive: defaultTheme === 'light' ? 'dark' : 'light',
+  });
 
   const toggleTheme = React.useCallback<ThemeToggle>(() => {
-    setTheme((theme) => (theme === 'dark' ? 'light' : 'dark'));
+    setTheme((theme) => ({
+      active: theme.inactive,
+      inactive: theme.active,
+    }));
   }, []);
 
   return (
