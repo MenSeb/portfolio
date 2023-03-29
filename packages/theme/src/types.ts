ee5303f4a31319761e15e277710710e59fbe2407
MenSeb <sebastien.menard.github@gmail.com>
2023-03-29 12:45:28 -0400

    refactor(types): export types for label and labelledby

--- packages/theme/src/types.ts
+++ packages/theme/src/types.ts
@@ -4,3 +4,13 @@ export type ThemeStore = {
   theme: Theme;
   toggleTheme: () => void;
 };
+
+export type Label = {
+  label?: string;
+  labelledby?: never;
+};
+
+export type Labelledby = {
+  label?: never;
+  labelledby?: string;
+};
