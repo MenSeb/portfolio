4aa36670fd341c339ee7d751a0787910fac90f6c
MenSeb <sebastien.menard.github@gmail.com>
2023-04-03 09:40:17 -0400

    feat(theme): create default theme value

--- packages/theme/src/constants.ts
+++ packages/theme/src/constants.ts
@@ -2,3 +2,4 @@ export const THEME_CONTEXT_ERROR =
   'useThemeContext should be used within ThemeProvider.';
 export const THEME_QUERY_DARK = '(prefers-color-scheme: dark)';
 export const THEME_STORAGE_KEY = 'color-scheme';
+export const THEME_DEFAULT = 'light';
