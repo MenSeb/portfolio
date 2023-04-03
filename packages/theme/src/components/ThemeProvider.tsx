12aa4d97e71e9c3d0c3d7d3497bc6bc238e36da5
MenSeb <sebastien.menard.github@gmail.com>
2023-04-03 09:41:24 -0400

    feat(provider): update provider with default theme and setters

--- packages/theme/src/components/ThemeProvider.tsx
+++ packages/theme/src/components/ThemeProvider.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
-import { THEME_QUERY_DARK, THEME_STORAGE_KEY } from '../constants';
-import { ThemeContext } from '../context';
 import { Theme } from '../types';
+import { ThemeContext } from '../context';
+import {
+  THEME_DEFAULT,
+  THEME_QUERY_DARK,
+  THEME_STORAGE_KEY,
+} from '../constants';
+
+export type ThemeProviderProps = React.PropsWithChildren & {
+  defaultTheme?: Theme;
+};
 
 export default function ThemeProvider({
   children,
-}: React.PropsWithChildren): JSX.Element {
+  defaultTheme = THEME_DEFAULT,
+}: ThemeProviderProps): JSX.Element {
   const refMediaQuery = React.useRef(window.matchMedia(THEME_QUERY_DARK));
 
   const [theme, setTheme] = React.useState<Theme>(() => {
@@ -15,6 +24,9 @@ export default function ThemeProvider({
 
     return refMediaQuery.current.matches ? 'dark' : 'light';
   });
+
+  const setThemeDark = React.useCallback(() => setTheme('dark'), []);
+  const setThemeLight = React.useCallback(() => setTheme('light'), []);
 
   const toggleTheme = React.useCallback(() => {
     setTheme((theme) => (theme === 'dark' ? 'light' : 'dark'));
@@ -39,7 +51,15 @@ export default function ThemeProvider({
   }, []);
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider
+      value={{
+        defaultTheme,
+        setThemeDark,
+        setThemeLight,
+        theme,
+        toggleTheme,
+      }}
+    >
       {children}
     </ThemeContext.Provider>
   );
