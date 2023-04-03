58b1f77957231a48fb4194a247037f05432d6beb
MenSeb <sebastien.menard.github@gmail.com>
2023-04-03 09:48:59 -0400

    chore(components): export components

--- packages/theme/src/components/index.ts
+++ packages/theme/src/components/index.ts
@@ -1,3 +1,8 @@
 export { default as ThemeButton } from './ThemeButton';
+export { default as ThemeButtonDark } from './ThemeButtonDark';
+export { default as ThemeButtonLight } from './ThemeButtonLight';
+export { default as ThemeButtonToggle } from './ThemeButtonToggle';
 export { default as ThemeIcon } from './ThemeIcon';
+export { default as ThemeIconDark } from './ThemeIconDark';
+export { default as ThemeIconLight } from './ThemeIconLight';
 export { default as ThemeProvider } from './ThemeProvider';
